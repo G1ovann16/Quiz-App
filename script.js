fac280e0620587adc7452cf20ad392fb870caf78
G1ovann16 <glandaburo92@gmail.com>
2021-02-08 20:55:40 +0100

    finish proyect Quiz app

--- script.js
+++ script.js
@@ -24,6 +24,8 @@ const quizData= [
     }
 ]
 
+const quiz = document.getElementById("quiz")
+const answerEls = document.querySelectorAll('.answer')
 const questionEl = document.getElementById('question')
 const a_text = document.getElementById('a_text');
 const b_text = document.getElementById('b_text');
@@ -32,10 +34,14 @@ const d_text = document.getElementById('d_text');
 const submiBtn = document.getElementById('submit');
 
 let currentQuiz=0;
+let score = 0;
 
 loadQuiz();
 
 function loadQuiz(){
+
+        deselectAnswers();
+
     const currentQuizData = quizData[currentQuiz];
     questionEl.innerText = currentQuizData.question;
      a_text.innerText = currentQuizData.a;
@@ -44,12 +50,36 @@ function loadQuiz(){
      d_text.innerText = currentQuizData.d;
 
     }
-    
-    submiBtn.addEventListener("click", ()=>{
-        currentQuiz++;
-    if(currentQuiz <  quizData.length){
-        loadQuiz();
-    }else{
-        alert("Ha terminado de responder todas las preguntas")
+
+    function getSelected(){
+        let answer = undefined;
+        answerEls.forEach(answerEl =>{
+            if(answerEl.checked){
+                answer=  answerEl.id    
+            }
+        });
+        return answer 
     }
-});
+    
+    function deselectAnswers(){
+        answerEls.forEach(answerEl =>{
+            answerEl.checked= false;
+        });
+    }
+
+    submiBtn.addEventListener("click", ()=>{
+
+        const answer = getSelected();
+    if(answer){
+        if(answer === quizData[currentQuiz].correct){
+             score++;   
+        }
+        currentQuiz++;
+        if(currentQuiz <  quizData.length){
+            loadQuiz();
+        }else{
+        quiz.innerHTML = `<h2>Ha respondido correctamente ${score}/${quizData.length} de las preguntas</h2>
+        <button onclick="location.reload()">Reiniciar</button>`;
+        }
+    }
+    });
